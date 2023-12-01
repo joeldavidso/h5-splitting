3e595a8b0f43946ab99aa54f0f444665d0b10cd1
joeldavidso <111747806+joeldavidso@users.noreply.github.com>
2023-12-01 15:46:22 +0000

    Update h5_splitter.py
    changed input_path for user input

--- h5_splitter.py
+++ h5_splitter.py
@@ -1,11 +1,13 @@
 import h5py
 import numpy as np
 
-input_path = "/storage/epp2/phubsg/FTag_samples/ttbar/"
+#path to directory with h5 file(s)
+input_path = ""
 
 input_h5s = ["user.lbezio.35269581._000001.output.h5",
 "user.lbezio.35269581._000002.output.h5",
 "user.lbezio.35269581._000003.output.h5"]
+
 
 #loops over all input h5 files
 for i in range(len(input_h5s)):
@@ -16,7 +18,7 @@ for i in range(len(input_h5s)):
 	#extracts the seperate datasets
 	datasets = [input['emtopo'],input['pflow']]
 	#creates new h5 files
-	emtopo_file = h5py.File('outputs/emtopo_'+str(i)+'.h5', 'w')
+	emtopo_file = h5py.File('outputs/emtopo_'+str(i+1)+'.h5', 'w')
 
 	#add datasets to output file
 	emtopo_file.create_dataset('jets',
